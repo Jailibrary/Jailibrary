5da3ede18bbb88a6627f40d2f496d0bca0188823
Jailibrary <49889517+Jailibrary@users.noreply.github.com>
2019-07-10 22:47:04 -0400

    Update routes.py

--- src/core/routes.py
+++ src/core/routes.py
@@ -1,6 +1,13 @@
 from flask import render_template, redirect, url_for
 from core import app
 
+is_maintenance_mode = True
+
+@app.before_request
+def check_for_maintenance():
+    if is_maintenance_mode and request.path != url_for('maintenance'): 
+        return redirect(url_for('maintenance'))
+
 @app.route("/")
 def home():
     return render_template("home.html", title="Home")
@@ -19,3 +26,7 @@ def developer():
 @app.route("/developer/info")
 def developer_info():
     return render_template("developer/info.html", title="Developer: Info")
+
+@app.route("/maintenance")
+def maintenance():
+    return 'Jailibrary is currently undergoing maintenance at this time. Please come back later.', 503
